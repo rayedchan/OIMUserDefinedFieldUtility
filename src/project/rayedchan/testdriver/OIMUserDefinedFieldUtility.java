3d1b2d52a6888b38f9d3ef19e4024f8cbbf3b01c
rayedchan <>
2014-04-02 21:11:23 -0400

    - Built Attribute Definition object with sample data - Tested create UDF method - Wrote method to parse document object to string

--- src/project/rayedchan/testdriver/OIMUserDefinedFieldUtility.java
+++ src/project/rayedchan/testdriver/OIMUserDefinedFieldUtility.java
@@ -8,6 +8,11 @@ import javax.security.auth.login.LoginException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
+import oracle.iam.configservice.api.Constants;
+import oracle.iam.configservice.vo.AttributeDefinition;
 import oracle.iam.identity.exception.UserSearchException;
 import oracle.iam.identity.usermgmt.api.UserManager;
 import oracle.iam.identity.usermgmt.vo.User;
@@ -15,6 +20,8 @@ import oracle.iam.platform.OIMClient;
 import oracle.iam.platform.authz.exception.AccessDeniedException;
 import oracle.iam.platform.entitymgr.vo.SearchCriteria;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import project.rayedchan.utilities.UDFUtility;
 
 /**
  *
@@ -30,7 +37,7 @@ public class OIMUserDefinedFieldUtility
     public static final String OIM_CLIENT_HOME = "/home/oracle/Desktop/oimclient";
     public static final String AUTHWL_PATH = OIM_CLIENT_HOME + "/conf/authwl.conf";
     
-    public static void main(String[] args) throws LoginException, AccessDeniedException, UserSearchException, ParserConfigurationException 
+    public static void main(String[] args) throws LoginException, AccessDeniedException, UserSearchException, ParserConfigurationException, TransformerConfigurationException, TransformerException, XPathExpressionException 
     {
         OIMClient oimClient = null;
         
@@ -55,14 +62,78 @@ public class OIMUserDefinedFieldUtility
    
             // Call a method from a service
             List<User> users = usermgr.search(new SearchCriteria("User Login", "*", SearchCriteria.Operator.EQUAL), new HashSet(), new HashMap());
-            System.out.println(users);
+            System.out.println(users);-
             */
             
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = dbf.newDocumentBuilder();
             Document doc = builder.newDocument();
             
+            // Root Element to hold all AttributeDefinition Elements
+            Element rootTag = doc.createElement("xl-ddm-data");
             
+            // Set properties of root element
+            rootTag.setAttribute("version", "2.0.2.1");
+            rootTag.setAttribute("user", "XELSYSADM");
+            rootTag.setAttribute("database", "jdbc:oracle:thin:@localhost:1521/orcl");
+            rootTag.setAttribute("exported-date", "1396307720581");
+            rootTag.setAttribute("description", "");
+            
+            // Add root element to document object
+            doc.appendChild(rootTag);
+            
+            // Built new Attribute Definition object which represents a UDF
+            AttributeDefinition newUDF = new AttributeDefinition();
+               
+            String api_name = "util_attr"; //
+            boolean isVisible = true;
+            boolean isBackendRequired = false;
+            String type = "string"; //
+            boolean isSystemControlled = false;
+            boolean isCustomAttribute = true;
+            String encryption = "CLEAR";
+            String description = "";
+            String backendType = type; 
+            boolean isMultiRepresented = false;
+            boolean isRequired = false;
+            Integer maxSize = 100; //
+            boolean isMultiValued = false;
+            boolean isUserSearchable = false;
+            boolean isReadOnly = false;
+            String  displayType = "TEXT"; //
+            boolean isBulkUpdatable = false;
+            boolean isMLS = false;
+            String  backendName = "usr_udf_util_attr"; //
+            boolean isSearchable = isUserSearchable; //
+            String  category = "Basic User Information";
+            
+            newUDF.setName(api_name);
+            newUDF.setVisible(isVisible);
+            newUDF.setBackendRequired(isBackendRequired);
+            newUDF.setType(type);
+            newUDF.setSystemControlled(isSystemControlled);
+            newUDF.setCustomAttribute(isCustomAttribute);
+            newUDF.setEncryption(encryption);
+            newUDF.setDescription(description);
+            newUDF.setBackendType(backendType);
+            newUDF.setMultiRepresented(isMultiRepresented);
+            newUDF.setRequired(isRequired);
+            newUDF.setMaxSize(maxSize);
+            newUDF.setMultiValued(isMultiValued);
+            newUDF.setUserSearchable(isUserSearchable);
+            newUDF.setReadOnly(isReadOnly);
+            newUDF.setDisplayType(displayType);
+            newUDF.setBulkUpdatable(isBulkUpdatable);
+            newUDF.setMLS(isMLS);
+            newUDF.setBackendName(backendName);
+            newUDF.setSearchable(isSearchable);
+            newUDF.setCategory(category);
+
+            // Add new UDF to document
+            UDFUtility.createUserDefinedField(doc, newUDF);
+            
+            // Print Document object to String
+            System.out.println(UDFUtility.parseDocumentIntoStringXML(doc));
             
         } 
             
